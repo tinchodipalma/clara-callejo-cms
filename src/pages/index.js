7ea2e3ed2bec7edeb0336158c0bd6556f637a0bc
Martin Di Palma <tincho.dipalma@gmail.com>
2019-08-08 18:26:39 -0300

    landing

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,11 +9,30 @@ const LAYOUT_CLASSES = {
   scrolling: 'HomeLayout HomeLayout--scroll',
 };
 
+const BANNERS_DATA = [
+  {
+    img: '/assets/banners/facebook.jpg',
+    label: 'Facebook',
+  },
+  {
+    img: '/assets/banners/instragram.jpg',
+    label: 'Instagram',
+  },
+  {
+    img: '/assets/banners/twitter.jpg',
+    label: 'Twitter',
+  },
+  {
+    img: '/assets/banners/network.jpg',
+    label: 'Otras Redes',
+  },
+];
+
 const IndexPage = () => {
   const [className, setClassName] = useState(LAYOUT_CLASSES.default);
 
   const onScroll = (event) => {
-    if (window.scrollY > window.innerHeight / 2) {
+    if (window.scrollY > 30) {
       setClassName(LAYOUT_CLASSES.scrolling);
     } else {
       setClassName(LAYOUT_CLASSES.default);
@@ -32,7 +51,8 @@ const IndexPage = () => {
     <Landing className={className}>
       <SEO title="Home" keywords={[`blog`, `gatsby`, `javascript`, `react`]} />
 
-      <div className="HomeSection">
+      <div className="HomeSection HomeSectionStartLayer">
+        <div></div>
         <div className="HomeSection__Main">
           <Typography variant="h3" color="secondary" className="Home__Slogan">
             Clara Callejo
@@ -40,6 +60,21 @@ const IndexPage = () => {
           <Typography variant="h5" className="Home__Slogan">
             Asesoramiento en Comunicación
           </Typography>
+        </div>
+      </div>
+
+      <div className="HomeSection HomeSectionBannersLayer">
+        <div className="HomeSection__Grid">
+          {BANNERS_DATA.map(({ img, label }, i) => (
+            <div key={i} className="HomeSection__Grid__Item">
+              <img
+                src={img}
+                alt={label}
+                className="HomeSection__Grid__Item__Image"
+              />
+              <div className="HomeSection__Grid__Item__Label">{label}</div>
+            </div>
+          ))}
         </div>
       </div>
     </Landing>
