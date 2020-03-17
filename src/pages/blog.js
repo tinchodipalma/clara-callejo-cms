84a0f45dd6f74fd40b0bacf6b30b303d35ac6d7d
Martin Di Palma <tincho.dipalma@gmail.com>
2020-03-17 18:42:42 -0300

    final changes

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -2,40 +2,70 @@ import React from 'react';
 import { Link, graphql } from 'gatsby';
 import Layout from '../layouts/Layout';
 import SEO from '../components/seo';
+import Aside from '../components/Aside/Aside';
+import { Typography } from '@material-ui/core';
 
-class Blog extends React.Component {
-  render() {
-    const { data } = this.props;
-    const siteTitle = data.allSiteJson.nodes[0].siteTitle;
-    const posts = data.allMarkdownRemark.edges;
+const Blog = ({ data, location }) => {
+  const siteTitle = data.allSiteJson.nodes[0].siteTitle;
+  const author = data.allSiteJson.nodes[0].author;
+  const posts = data.allMarkdownRemark.edges;
 
-    return (
-      <Layout location={this.props.location} title={siteTitle}>
-        <SEO title="Blog" />
-        <div style={{ margin: '20px 0 40px' }}>
-          {posts.map(({ node }) => {
-            const title = node.frontmatter.title || node.fields.slug;
-            return (
-              <div key={node.fields.slug}>
-                <h3>
-                  <Link style={{ boxShadow: `none` }} to={node.fields.slug}>
-                    {title}
-                  </Link>
-                </h3>
-                <small>{node.frontmatter.date}</small>
-                <p
-                  dangerouslySetInnerHTML={{
-                    __html: node.frontmatter.description || node.excerpt,
-                  }}
-                />
-              </div>
-            );
-          })}
+  return (
+    <Layout location={location} title={siteTitle}>
+      <SEO title="Blog" />
+      <div className="BlogPage BlogPage__Container BlogPage__WithAside">
+        <div className="BlogPage__MainContainer">
+          <div className="BlogPage__Title MainSection__Title">
+            <Typography variant="h4" color="secondary">
+              Blog
+            </Typography>
+          </div>
+          <div className="BlogPage__Content">
+            {posts.map(({ node }) => (
+              <Link
+                key={node.fields.slug}
+                to={node.fields.slug}
+                className="BlogPage__Content__Item"
+              >
+                <Typography
+                  variant="h6"
+                  color="secondary"
+                  className="BlogPage__Content__Item__Title"
+                >
+                  {node.frontmatter.title}
+                </Typography>
+
+                <div className="BlogPage__Content__Item__Data">
+                  <div className="BlogPage__Content__Item__Image">
+                    <img
+                      src={node.frontmatter.image}
+                      alt={node.frontmatter.title}
+                    />
+                  </div>
+
+                  <div
+                    className="BlogPage__Content__Item__Description"
+                    dangerouslySetInnerHTML={{
+                      __html: node.frontmatter.description || node.excerpt,
+                    }}
+                  />
+                </div>
+                <div className="BlogPage__Content__Item__Metadata">
+                  <small>{author}</small>
+                  <small>{node.frontmatter.date}</small>
+                </div>
+              </Link>
+            ))}
+          </div>
         </div>
-      </Layout>
-    );
-  }
-}
+
+        <aside className="BlogPage__Aside">
+          <Aside />
+        </aside>
+      </div>
+    </Layout>
+  );
+};
 
 export default Blog;
 
@@ -43,6 +73,7 @@ export const pageQuery = graphql`
   query {
     allSiteJson {
       nodes {
+        author
         siteTitle
       }
     }
@@ -62,6 +93,7 @@ export const pageQuery = graphql`
             date(formatString: "DD/MM/YYYY")
             title
             description
+            image
           }
         }
       }
